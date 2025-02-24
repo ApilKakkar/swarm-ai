524e470be65c8bfaf843ad67c363471bb2ddb5b8
Apil Kakkar <apilkakkar@Apils-MacBook-Air.local>
2025-02-24 23:47:42 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className='h-[100vh] w-full bg-black text-white text-4xl flex items-center justify-center'>
+        SWARM-AI
+      </div>
+    </>
+  )
+}
+
+export default App
